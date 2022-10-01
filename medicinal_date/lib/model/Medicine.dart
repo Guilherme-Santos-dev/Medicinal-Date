4036971dd6e116af8c463990f1b27ce6f019f69c
Guilherme <guilhermeciadope@gmail.com>
2022-10-01 15:45:21 -0300

    Update Medicine.dart

--- medicinal_date/lib/model/Medicine.dart
+++ medicinal_date/lib/model/Medicine.dart
@@ -1,14 +1,30 @@
 import 'dart:convert';
 class Medicine{
+  int? id;
   String nome;
   String obscervacao;
   String quantidade;
   String horario;
+  bool isFavorite;
 
   Medicine({
+    this.id,
     required this.nome,
     required this.obscervacao,
     required this.quantidade,
     required this.horario,
+    required this.isFavorite,
   });
+
+  Map<String, dynamic> toMap(){
+  return <String, dynamic>{
+    'id': id,
+    'nome': nome,
+    'obscervacao': obscervacao,
+    'quantidade': quantidade,
+    'horario': horario,
+    'isFavorite': isFavorite,
+
+  };
+  }
 }
